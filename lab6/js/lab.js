ec0e25938afffe063f73211526aee90e4f8585bb
Ammyrin <alexchen89550@gmail.com>
2024-04-29 00:11:08 -0700

    Lab 6 Done

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,11 +1,24 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Alexander Chen (achen197@ucsc.edu)
+// Date: April 29th
 
 // Constants
 
 // Functions
+myTransport = ["Honda Civic", "Bike", "Train", "Bus"];
 
+myMainRide = {
+  make: "Honda",
+  model: "Civic",
+  color: "Blue",
+  year: 2022,
+  age: function() {
+    return 2024 - this.year;
+  }
+}
+document.writeln("Transportation I Use: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>",
+JSON.stringify(myMainRide, null, '\t'), "</pre");
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
