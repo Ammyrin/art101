135751a29a6f959e73f4c475be2f375694a0afa4
Ammyrin <alexchen89550@gmail.com>
2024-05-17 00:00:25 -0700

    Lab 10 Fixed

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -4,7 +4,6 @@
 
 function generateRandomText() {
   const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-  
   const min = 3;
   const max = 100;
   const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -23,5 +22,4 @@ const newText = generateRandomText();
 
 // append a new div to our output div
 $("#output").append('<div class="text"><p>' + newText + '</p></div>');
-
 });
